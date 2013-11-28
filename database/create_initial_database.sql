b9a126657d567f07244d89978979d07e8abbcfe2
Dean Lozo <deanl@mesan.no>
2013-11-28 14:30:05 +0100

    Created sql scripts to create the initial database and tables. Created a script that should be used to populate the db.

---
+++ database/create_initial_database.sql
@@ -0,0 +1,57 @@
+-- -----------------------------------------------------
+-- Create kad schema
+-- -----------------------------------------------------
+
+CREATE SCHEMA kad DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE kad;
+
+-- -----------------------------------------------------
+-- Create users table
+-- -----------------------------------------------------
+CREATE TABLE users(
+  users_username    VARCHAR(30),
+  users_email       VARCHAR(100)  NOT NULL   UNIQUE ,
+  user_password     VARCHAR(255)  NOT NULL,
+  user_salt         VARCHAR(255)  NOT NULL,
+  user_fullname     VARCHAR(255)  NOT NULL,
+  user_country      VARCHAR(255)  NOT NULL,
+  user_countryCode  VARCHAR(50)   NOT NULL,
+
+  PRIMARY KEY (users_username)
+);
+
+-- -----------------------------------------------------
+-- Create userGroups table
+-- ----------------------------------------------------
+
+CREATE TABLE userGroups(
+  userGroups_userGroup    VARCHAR(100),
+
+  PRIMARY KEY (userGroups_userGroup)
+);
+
+-- -----------------------------------------------------
+-- Create userGroups table
+-- -----------------------------------------------------
+
+INSERT INTO userGroups
+  (userGroups_userGroup)
+VALUES
+('admin'),
+('moderator'),
+('user');
+
+-- -----------------------------------------------------
+-- Create userInUserGroup table
+-- -----------------------------------------------------
+
+CREATE TABLE userInUserGroup(
+  usersInUserGroup_username     VARCHAR(30),
+  usersInUserGroup_userGroup    VARCHAR(100),
+
+  PRIMARY KEY (usersInUserGroup_username, usersInUserGroup_userGroup),
+  FOREIGN KEY (usersInUserGroup_username)
+    REFERENCES users(users_username),
+  FOREIGN KEY (usersInUserGroup_userGroup)
+    REFERENCES userGroups(userGroups_userGroup)
+)
---
+++ database/populate_database.sql
@@ -0,0 +1,9 @@
+-- -----------------------------------------------------
+-- Populate users table
+-- -----------------------------------------------------
+USE kad;
+INSERT INTO users
+  (users_username, users_email, user_password, user_salt, user_fullname, user_country, user_countryCode)
+VALUES
+('admin','dummy@dummymail.com','f8b7f4089381a43b15089059c7780841ebd1986497eb44a2c968d9c06d973213','','Hestemannen',
+ 'Norway','no_NO');
