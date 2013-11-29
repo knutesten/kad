76789f0db3b46018f505272ad9aa172ad7f3c9ff
Knut Esten Melandsø Nekså <knutm@mesan.no>
2013-11-29 14:14:37 +0100

    Enabled faces

--- src/main/java/no/mesan/controllers/NewUserController.java
+++ src/main/java/no/mesan/controllers/NewUserController.java
@@ -1,7 +1,10 @@
 package no.mesan.controllers;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
+import no.mesan.model.User;
 import no.mesan.persistence.UserDao;
 
 /**
@@ -9,10 +12,14 @@ import no.mesan.persistence.UserDao;
  *
  * @author Knut Esten Melandsø Nekså
  */
+@Named
+@RequestScoped
 public class NewUserController {
     @Inject
     private UserDao userDao;
 
     public void registerNewUser() {
+        final User newUser = new User("username", "email", "password");
+        userDao.createUser(newUser);
     }
 }
