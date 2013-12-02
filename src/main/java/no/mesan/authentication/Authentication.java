720969d36ea7e927ba893c7ccf53e5d5bee0854e
Knut Esten Melandsø Nekså <knutm@mesan.no>
2013-12-02 15:51:56 +0100

    Changed password to use String instead of char[]

--- src/main/java/no/mesan/authentication/Authentication.java
+++ src/main/java/no/mesan/authentication/Authentication.java
@@ -1,9 +1,7 @@
 package no.mesan.authentication;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -24,19 +22,16 @@ public class Authentication {
         return new BigInteger(1, salt).toString(16);
     }
 
-    public String generatePasswordHash(final char[] newPassword, final String salt) {
+    public String generatePasswordHash(final String newPassword, final String salt) {
         try {
-            final Charset charset = Charset.forName("UTF-8");
-            final ByteBuffer passwordAsBytes = charset.encode(CharBuffer.wrap(newPassword));
-
             final MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(DatatypeConverter.parseHexBinary(salt));
-            md.update(passwordAsBytes.array());
+            md.update(newPassword.getBytes("UTF-8"));
 
             final BigInteger bigInt = new BigInteger(1, md.digest());
             return bigInt.toString(16);
-        } catch(NoSuchAlgorithmException nsae) {
-            nsae.printStackTrace();
+        } catch(NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
         return null;
     }
