bc4f2e3c25ea97ac2915bdca6c26275263ef35b5
Anders G. Moe <andersm@mesan.no>
2013-12-11 13:49:28 +0100

    added some more topics into the populate files for testing

--- database/populate_tables/populate_categories_tables.sql
+++ database/populate_tables/populate_categories_tables.sql
@@ -3,5 +3,8 @@ INSERT INTO categories (category_name) VALUES ('Hester'),
                                               ('Kameler');
 
 INSERT INTO topicInCategory VALUES
-  (1, 1),
-  (2, 2);
+  (2, 1),
+  (1, 2),
+  (2, 3),
+  (2, 4),
+  (2, 5);
