2fede245cfe05c01f6764a189852bd35967af8bf
Knut Esten Melandsø Nekså <knutm@mesan.no>
2013-12-11 15:08:31 +0100

    Added programmatic login

---
+++ src/main/java/no/mesan/controllers/LoginController.java
@@ -0,0 +1,58 @@
+package no.mesan.controllers;
+
+import java.io.IOException;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import no.mesan.model.User;
+import no.mesan.persistence.user.UserDao;
+
+/**
+ * TODO
+ *
+ * @author Knut Esten Melandsø Nekså
+ */
+@Named
+@RequestScoped
+public class LoginController {
+    private String username;
+    private String password;
+
+    @Inject
+    private UserDao userDao;
+
+    public void login() throws IOException {
+        final ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        final HttpServletRequest request = (HttpServletRequest) context.getRequest();
+        try {
+            request.login(username, password);
+            final User user = userDao.getUserByUsername(username);
+            context.getSessionMap().put("user", user);
+        } catch (ServletException se) {
+            System.out.println(se.getMessage());
+        }
+        context.redirect("/index.jsf");
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+}
