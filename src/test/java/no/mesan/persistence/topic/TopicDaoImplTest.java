47ef813e9195ad2936cbf1b9e75c7bf2726ec84e
Anders G. Moe <andersm@mesan.no>
2013-12-13 14:30:45 +0100

    added tests for getNumberOfTopicsInCategory

--- src/test/java/no/mesan/persistence/topic/TopicDaoImplTest.java
+++ src/test/java/no/mesan/persistence/topic/TopicDaoImplTest.java
@@ -169,7 +169,6 @@ public class TopicDaoImplTest {
         final Category category = mock(Category.class);
         when(category.getId()).thenReturn(1);
         final String title     = "Hester er kule";
-        final Date  createdTime = new Date();
         final Topic newTopic   = new Topic(title, hestemann);
         topicDao.createTopic(newTopic, category);
 
@@ -204,5 +203,21 @@ public class TopicDaoImplTest {
         final int numberOfPostsInTopicThatDoesNotExist = 0;
         final int numberOfPostsInTopicThatDoesNotExistFromDatabase = topicDao.getNumberOfPostsInTopic(topicId);
         assertEquals(numberOfPostsInTopicThatDoesNotExist, numberOfPostsInTopicThatDoesNotExistFromDatabase);
+    }
+    
+    @Test
+    public void getNumberOfTopicsInCategoryShouldReturnSevenWhenThereAreSevenTopicsInACategory() {
+        final Category category = new Category(1, "Hester", HESTER_ER_FINE);
+        final int expectedNumberOfTopics = 7;
+        final int numberOfTopicsInHesterCategoryFromDatabase = topicDao.getNumberOfTopicsInCategory(category);
+        assertEquals(expectedNumberOfTopics, numberOfTopicsInHesterCategoryFromDatabase);
+    }
+    
+    @Test
+    public void getNumberOfTopicsInCategoryShouldReturn0IfTheCategoryDoesNotExist() {
+        final Category category = new Category(13131313, "No Category", null);
+        final int expectedNumberOfTopics = 0;
+        final int numberOfTopicsInCategoryFromDatabase = topicDao.getNumberOfTopicsInCategory(category);
+        assertEquals(expectedNumberOfTopics, numberOfTopicsInCategoryFromDatabase);
     }
 }
